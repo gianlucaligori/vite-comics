0b5c2b628d171b892236b36b1cba6c7433f5c649
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-05-18 15:36:36 +0200

    Start

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import Content from './components/Content.vue';
+import Footer from './components/Footer.vue';
+import Header from './components/Header.vue';
+
+export default {
+  components: {
+    Header,
+    Content,
+    Footer,
+  },
+};
+
+</script>
+
+<template>
+  
+</template>
+
+<style>
+</style>
