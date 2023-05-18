a71c23fbcfafca247c6a9f94177f90b73bf26be2
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-05-18 18:03:57 +0200

    add ul footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,11 +3,34 @@
 
 <template>
      <footer>
-       <a href="">Digital comics</a>
-       <a href="">DC Merchandise</a>
-       <a href="">Subscription</a>
-       <a href="">Comic Shop Locator</a>
-       <a href="">DC POWER VISA</a>
+        <div class="logos_links" >
+            <a href="">Digital comics</a>
+            <a href="">DC Merchandise</a>
+            <a href="">Subscription</a>
+            <a href="">Comic Shop Locator</a>
+            <a href="">DC POWER VISA</a>
+        </div>
+
+
+       <div class="footer_links">
+            <ul>
+                <h4>DC COMICS</h4>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+            </ul>
+
+            <ul>
+                <h4>SHOP</h4>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+
+            </ul>
+       </div>
      </footer>
 </template>
 
@@ -20,9 +43,32 @@ footer {
     @include footer
 }
 
-a {
-    @include a
+.logos_links {
+    @include logos_links
 }
 
+a {
+    @include a_container
+}
+
+.footer_links {
+    background-image: url(../assets/img/footer-bg.jpg);
+    height: 300px;
+}
+
+h3 {
+    margin-bottom: .5rem;
+}
+
+ul {
+    color: white;
+    list-style:none;
+    padding: 1rem;
+}
+
+li {
+    padding: .15rem;
+    font-size: .8rem;
+}
 
 </style>
